e99ac56f787ff2aee57cd74a088fe697fda05246
alangarciac <alangarciacamina@gmail.com>
2021-02-09 17:40:09 -0300

    initial scaffolding

---
+++ src/main/java/com/app/rest/app/AppRest.java
@@ -0,0 +1,34 @@
+package com.app.rest.app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class AppRest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRest.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+}

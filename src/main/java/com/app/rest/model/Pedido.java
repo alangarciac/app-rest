4b8dd19cd8c2ab2b1f8e0f7432c0d781827349cd
tato <cpiazza@cespi.unlp.edu.ar>
2021-02-14 21:38:50 -0300

    modelo creado

---
+++ src/main/java/com/app/rest/model/Pedido.java
@@ -0,0 +1,32 @@
+package com.app.rest.model;
+
+public class Pedido {
+
+    private String nombre;
+    private Integer id;
+    private boolean vegano;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public boolean isVegano() {
+        return vegano;
+    }
+
+    public void setVegano(boolean vegano) {
+        this.vegano = vegano;
+    }
+}

eaeac93c35117448b4481cf41d2e58a69cc7ab60
tato <cpiazza@cespi.unlp.edu.ar>
2021-02-15 00:23:45 -0300

    sleep agregado

--- src/main/java/com/app/rest/service/ServicioPedido.java
+++ src/main/java/com/app/rest/service/ServicioPedido.java
@@ -1,9 +1,12 @@
 package com.app.rest.service;
 
 import com.app.rest.model.Pedido;
+import org.springframework.retry.backoff.Sleeper;
+import org.springframework.retry.support.RetrySimulation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ServicioPedido {
 
@@ -11,6 +14,9 @@ public class ServicioPedido {
         List<Pedido> pedidos= new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             pedidos.add(new Pedido(i, "nombre"+i, false));
+        }
+        for (int i = 0; i < 5; i++) {
+            TimeUnit.SECONDS.sleep(5);
         }
         return pedidos;
     }

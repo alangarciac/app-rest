ba9136dcc6cf78e76b7b12d7122689b33d7c7375
tato <cpiazza@cespi.unlp.edu.ar>
2021-02-22 17:07:50 -0300

    modifique algo que estaba de mas

--- src/main/java/com/app/rest/controller/AppController.java
+++ src/main/java/com/app/rest/controller/AppController.java
@@ -1,11 +1,5 @@
 package com.app.rest.controller;
 
-import com.app.rest.model.Item;
-import com.app.rest.services.ItemService;
-import com.app.rest.services.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,21 +10,6 @@ class AppController {
         return "Greetings from Spring Boot!";
     }
 
-    @Autowired
-    OrderService order;
-    @Autowired
-    ItemService item;
-
-
-    //Returns all orders
-    @GetMapping("/orders")
-    public ResponseEntity<Object> orders(){
-        try {
-            item.save(new Item("verga","verga","verga"));
-            return new ResponseEntity<Object>(order.getOrders(), HttpStatus.OK);
-        } catch (InterruptedException e) {
-            return new ResponseEntity<Object>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
 
 }
